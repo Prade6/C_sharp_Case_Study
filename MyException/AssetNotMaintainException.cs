bb5ec9ebee58c083f5a8aa47ad0a60daf8efbf32
PRADEEPA S <141475813+Prade6@users.noreply.github.com>
2024-05-22 09:30:46 +0530

    Add files via upload

---
+++ MyException/AssetNotMaintainException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Asset_management.MyException
+{
+    internal class AssetNotMaintainException:ApplicationException
+    {
+        public AssetNotMaintainException(string message) : base(message) { }
+    }
+}
